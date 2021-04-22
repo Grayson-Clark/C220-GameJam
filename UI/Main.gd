19ae67408438f6a5e78f576e01abb2e834894a9e
jmchurch <jmchurch@iu.edu>
2021-04-22 02:44:12 -0400

    finished main menu

--- UI/Main.gd
+++ UI/Main.gd
@@ -1,14 +1,34 @@
 extends Control
 
+var play = false
+var quit = false
 
-func _ready():
-	pass
+func _physics_process(delta):
+	if play:
+		play = false
+		get_tree().change_scene("res://Game.tscn")
+	if quit:
+		quit = false
+		get_tree().quit()
+		
 
 
 
 func _on_Play_pressed():
-	get_tree().change_scene("res://Game.tscn")
+	$Play_Timer.start()
+	$Nothing_Pressed.hide()
+	$Play_Pressed.show()
 
 
 func _on_Quit_pressed():
-	get_tree().quit()
+	$Quit_Timer.start()
+	$Nothing_Pressed.hide()
+	$Quit_Pressed.show()
+
+
+func _on_Play_Timer_timeout():
+	play = true
+
+
+func _on_Quit_Timer_timeout():
+	quit = true
