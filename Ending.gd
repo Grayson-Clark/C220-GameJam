37997732399b6870c4ece1c20b394d463c2d798a
jmchurch <jmchurch@iu.edu>
2021-04-24 16:39:34 -0400

    background in end fades out goes to start

--- Ending.gd
+++ Ending.gd
@@ -4,3 +4,8 @@ func _ready():
 	var Music = get_node("/root/Music")
 	Music.end_music()
 	$Ascension.play()
+	
+
+
+func _on_ColorRect_animation_finished(anim_name):
+	get_tree().change_scene("res://UI/Main.tscn")
