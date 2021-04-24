ee729282268f39a6a14292d8b237b5ba094eaa39
jmchurch <jmchurch@iu.edu>
2021-04-24 17:22:30 -0400

    finished

--- Level/LevelAssets/Portals/LevelEnd.gd
+++ Level/LevelAssets/Portals/LevelEnd.gd
@@ -2,19 +2,25 @@ extends Node2D
 
 var make_vis = false
 onready var label = $Label
-
 func _ready():
 	if label.percent_visible != 0:
 		label.percent_visible = 0
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
-		if Global.num_spirits >= 8:
+		if Global.num_spirits >= 9:
 			print("Game complete!")
-			get_tree().change_scene("res://Ending.tscn")
+			$ColorRect/Fade.play("Fade")
 		else:
 			make_vis = true
 
 func _process(_d):
 	if make_vis:
 		label.percent_visible += 0.005
+
+
+
+
+
+func _on_Fade_animation_finished(anim_name):
+	get_tree().change_scene("res://Ending.tscn")
